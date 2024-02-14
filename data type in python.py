d6d74ecc2e9a0865d5be166c1e20c861cf8de6aa
Rajeev Sharma <115711628+machphy@users.noreply.github.com>
2024-02-14 20:57:27 +0530

    Add files via upload

---
+++ data type in python.py
@@ -0,0 +1,44 @@
+int 100 , 50
+float decimal no -- 2.02 , 5.1
+complex no -- real ans img  1+2j , 6i+7int
+
+
+text data type ---- string (""  '')
+"data"  'data'
+
+boolean true / false
+
+sequence data type ----
+
+list  ----sequance of some int
+[1,2,32,4]
+["kuch bhi ", "sbcks"]
+ and tuple  -collection of items (1,2,2,3,5)
+
+
+
+set data type
+
+
+unorderd collection of dataclasses
+sets
+= {"akbs", "kabckA", "KAJCBSKASFB" }
+
+Nothing = null
+
+Ascii --- Amercian Standard code for information interchange (it help tp represent char and numaric code)
+    A-Z   65-90
+A-65
+B-66
+Z-90
+
+a-z 97-122
+
+0-9   48-57
+space_____  32
+
+and unicode values ---
+
+
+
+ord() function
---
+++ input.py
@@ -0,0 +1,15 @@
+name=input("enter your name :  ")
+print(name)
+
+#always capture as string
+
+
+#type casting -- converting one data type to another
+
+'''ex --'''
+
+age = int( input("enter your age : "))
+print(type(age))
+
+no1=input("enter first no : ")
+print(no1)
