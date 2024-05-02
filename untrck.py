9d37ffae99adc40dd43338424679ebb017bec967
Rajeev Sharma <rajeevsharmamachphy@gmail.com>
2024-05-02 11:45:14 +0530

    bcvcjhdbfjwbfwe

--- untrck.py
+++ untrck.py
@@ -1,9 +1,7 @@
 
-# Outer loop
 for i in range(3):
-    # Inner loop
+   
     for j in range(3):
-        # Perform some action
+      
         print(f"Outer loop: {i}, Inner loop: {j}")
 
-# Continue with the rest of your code
