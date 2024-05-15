7653dd761a0b168b94fbf4f9bfd3faeab631dce5
Rajeev Sharma <rajeevsharmamachphy@gmail.com>
2024-05-15 18:01:48 +0530

    new update

---
+++ section5/conditional.py
@@ -0,0 +1,5 @@
+a=int(input('enter a number: '))
+if a>0:
+    print('positive')
+else:
+    print('negative')
---
+++ section5/gender_check.py
@@ -0,0 +1,6 @@
+gender=input('Enter your gender :- ')
+if gender=='m'or gender=='M':
+    print('Male')
+else:
+    print('Female')
+    
