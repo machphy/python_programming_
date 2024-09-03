dce5bb5b04b9dc90098619633cd36889c62fcd07
Rajeev Sharma <rajeevsharmamachphy@gmail.com>
2024-09-03 15:00:55 +0530

    new program

---
+++ list/murge_two_list.py
@@ -0,0 +1,9 @@
+# Define two lists
+list1 = [1, 2, 3, 4]
+list2 = [5, 6, 7, 8]
+
+# Merge the two lists using concatenation
+merged_list = list1 + list2
+
+# Display the merged list
+print("Merged List:", merged_list)
