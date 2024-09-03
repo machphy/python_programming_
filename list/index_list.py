2e7db090f2b65a56d6544840d8bcea45b380f696
Rajeev Sharma <rajeevsharmamachphy@gmail.com>
2024-09-03 20:36:16 +0530

    new codde

---
+++ list/index_list.py
@@ -0,0 +1,7 @@
+my_list = [10, 20, 30, 20, 40]
+index_value = my_list.index(20)
+print("Index of 20:", index_value)
+
+my_list = [10, 20, 30, 20, 40]
+index_value = my_list.index(20, 2)
+print("Index of 20 starting from index 2:", index_value)
