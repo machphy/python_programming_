fce5e9781bfa99358f4aef4d1f8eeeb42329207b
Rajeev Sharma <rajeevsharmamachphy@gmail.com>
2024-09-08 14:58:29 +0530

    ok

---
+++ tuple/tupleanother.py
@@ -0,0 +1,8 @@
+# l1=(x for x in range(10))
+# print(l1)
+
+
+# <generator object <genexpr> at 0x00000166B504CAC0>
+
+l1=tuple(x for x in range(10))
+print(l1)
---
+++ tuple/tuplebracketexample.py
@@ -0,0 +1,2 @@
+t2=*(x for x in range (10))
+print(t2)
