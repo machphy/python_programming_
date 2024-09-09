98e5056d66372b41ab8385abff3a19c214cbcada
Rajeev Sharma <rajeevsharmamachphy@gmail.com>
2024-09-09 07:51:01 +0530

    ok

--- sets/urder_set.py
+++ sets/urder_set.py
@@ -1,2 +1,12 @@
-set={25,45,78,10,32,41,123,10,142,12,14,15}
-print(set)
+# set={25,45,78,10,32,41,123,10,142,12,14,15}
+# print(set)
+
+
+
+# value cant be change 
+# first replace and then update new 
+
+lst = [25, 45, 78, 10, 32, 41, 123, 10, 142, 12, 14, 15]
+lst[2] = 50
+print(lst[2])
+print(lst)
