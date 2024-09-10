f0142a7088195dc552f9d7945e1bb365ad62860f
Rajeev Sharma <rajeevsharmamachphy@gmail.com>
2024-09-10 23:05:37 +0530

    sdnbfs

--- swap2.py
+++ swap2.py
@@ -1,41 +1,16 @@
-# Wap to Swap 2 Numbers without using Thired/Temp Variable
-
-# Hardcoding: whenevr you give direct values to your variables
-#             this process is called Hardcoding
-
-# logic based Question
-
-
 A = 20
-
 B = 45
-
 print("Before Swap Value of A is = ", A)
 print("Before Swap Value of B is = ", B)
-
-# logic For Swap 2 Values Without Temp 
-
-A = A + B    # A = 20+45 => A = 65
-
-B = A - B    # B = 65-45 => B = 20
-
-A = A - B    # A = 65-20 => A = 45
-
-
+A = A + B    
+B = A - B    
+A = A - B    
 print("After Swap Value of A is = ", A)
 print("After Swap Value of B is = ", B)
-
-
-
 C = 80
-
 D = 75
-
 print("Before Swap Value of C is = ", C)
 print("Before Swap Value of D is = ", D)
-
 C , D = 75 , 80
-
-
 print("After Swap Value of C is = ", C)
 print("After Swap Value of D is = ", D)
