0fb108942bc0474e33ef8be5a2a55d3e0cf6f4e7
Rajeev Sharma <rajeevsharmamachphy@gmail.com>
2024-09-10 23:02:32 +0530

    dsdf

--- Area_Triangle.py
+++ Area_Triangle.py
@@ -1,9 +1,3 @@
-# Wap to Find Area of Triangle
-# Formula Based Questions
-# Hardcoding: whenevr you give direct values to your variables this process is called Hardcoding
-
-# Area = (0.5) * Base * Height
-
 Base = 20
 
 Height = 40
