939de8b3ae7f3fce7574a006c60cc72a4887d732
Rajeev Sharma <rajeevsharmamachphy@gmail.com>
2024-09-10 23:04:34 +0530

    dsf

--- swap1.py
+++ swap1.py
@@ -1,29 +1,11 @@
-# Wap to Swap 2 Numbers
-
-# Hardcoding: whenevr you give direct values to your variables
-#             this process is called Hardcoding
-
-# logic based Question
-
-
 A = 20
-
 B = 45
-
 Temp = 0
-
 print("Before Swap Value of A is = ", A)
 print("Before Swap Value of B is = ", B)
-
-# logic For Swap 2 Values
-
-Temp = A    # Temp = 20
-
-A = B       # A = 45
-
-B = Temp    # B = 20
-
-
+Temp = A    
+A = B       
+B = Temp    
 print("After Swap Value of A is = ", A)
 print("After Swap Value of B is = ", B)
 
