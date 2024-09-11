7b31c1a72193b4a2a3b6b0f632b5e9d7ba4e5767
Rajeev Sharma <rajeevsharmamachphy@gmail.com>
2024-09-11 11:39:28 +0530

    somting update

--- list/loopusing_range.py
+++ list/loopusing_range.py
@@ -1,4 +1,4 @@
-list1=[1,2,3,4,5,6,7,8,9]
+list1=[1,2,3,4,5,6,5,8,9]
 for i in range(len(list1)):
     print(list1[i])
     
--- list/murge_two_list.py
+++ list/murge_two_list.py
@@ -1,6 +1,6 @@
 
 list1 = [1, 2, 3, 4]
-list2 = [5, 6, 7, 8]
+list2 = [5, 6, 7, 85]
 
 
 merged_list = list1 + list2
