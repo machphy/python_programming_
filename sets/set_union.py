74e36cf054f7253f358efa3af17ebe45fd8722ab
Rajeev Sharma <rajeevsharmamachphy@gmail.com>
2024-09-13 22:53:23 +0530

    ok

---
+++ sets/set_union.py
@@ -0,0 +1,8 @@
+A={55,14,0,12,0,12,0,4,52,1,1,56,31,5}
+B={3,4,6,4,84,6,58,5,64,3,6,46,58,6,5}
+A.union(B)
+A.intersection(B)
+A.difference(B)
+A.symmetric_difference(B)
+
+print(A.union(B) , A.intersection(B) ,A.difference(B) ,A.symmetric_difference(B))
