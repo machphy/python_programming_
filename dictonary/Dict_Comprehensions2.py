65ad34ccea1f63ad0e6629c89ff5bbef2950ef8e
Rajeev Sharma <rajeevsharmamachphy@gmail.com>
2024-09-18 20:50:08 +0530

    ok

---
+++ dictonary/Dict_Comprehensions2.py
@@ -0,0 +1,11 @@
+person={
+    "name":"kuchbhi",
+    "age":30,
+    "city":"new yerk"
+}
+
+squares = {x: x*x for x in range(6)}
+
+# Dictionary comprehension with a condition
+even_squares = {x: x*x for x in range(10) if x % 2 == 0}
+print(even_squares)  # Output: {0: 0, 2: 4, 4: 16, 6: 36, 8: 64}
