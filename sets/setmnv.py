758ff12c949528a35317ac30c4e3189b49e32bcd
Rajeev Sharma <rajeevsharmamachphy@gmail.com>
2024-09-18 19:08:23 +0530

    ok

--- sets/setmnv.py
+++ sets/setmnv.py
@@ -1,9 +1,2 @@
 squares = {x ** 2 for x in range(10)}
 print(squares)
-print(kucvfewkbfi)
-squares = {x ** 2 for x in range(10)}
-print(squares)
-print(kucvfewkbfi)
-squares = {x ** 2 for x in range(10)}
-print(squares)
-print(kucvfewkbfi)
