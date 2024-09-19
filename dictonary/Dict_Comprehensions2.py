5638aa227701b0aee0c312876bdc6556d1d278bd
Rajeev Sharma <rajeevsharmamachphy@gmail.com>
2024-09-19 13:24:59 +0530

    jsbfuds

--- dictonary/Dict_Comprehensions2.py
+++ dictonary/Dict_Comprehensions2.py
@@ -8,4 +8,4 @@ squares = {x: x*x for x in range(6)}
 
 # Dictionary comprehension with a condition
 even_squares = {x: x*x for x in range(10) if x % 2 == 0}
-print(even_squares)  # Output: {0: 0, 2: 4, 4: 16, 6: 36, 8: 64}
+print(even_squares)  # Output: {0: 0, 2: 4, 4: 16, 6: 36, 845454: 64}
