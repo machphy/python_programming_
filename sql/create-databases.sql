6792e8844f65eff93c53500d58331c711f2276df
Rajeev Sharma <rajeevsharmamachphy@gmail.com>
2024-09-25 07:30:16 +0530

    SQL

--- sql/create-databases.sql
+++ sql/create-databases.sql
@@ -302,7 +302,6 @@ INSERT INTO `products` VALUES (3,'Lettuce - Romaine, Heart',38,3.35);
 INSERT INTO `products` VALUES (4,'Brocolinni - Gaylan, Chinese',90,4.53);
 INSERT INTO `products` VALUES (5,'Sauce - Ranch Dressing',94,1.63);
 INSERT INTO `products` VALUES (6,'Petit Baguette',14,2.39);
-INSERT INTO `products` VALUES (7,'Sweet Pea Sprouts',98,3.29);
 INSERT INTO `products` VALUES (8,'Island Oasis - Raspberry',26,0.74);
 INSERT INTO `products` VALUES (9,'Longan',67,2.26);
 INSERT INTO `products` VALUES (10,'Broom - Push',6,1.09);
