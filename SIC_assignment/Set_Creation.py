00578ee4175b52f378c93695c3cc61c222ae5f07
Rajeev Sharma <rajeevsharmamachphy@gmail.com>
2024-10-08 21:07:57 +0530

    k

--- SIC_assignment/Set_Creation.py
+++ SIC_assignment/Set_Creation.py
@@ -1,4 +1,3 @@
-
-prime_numbers = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29}
-print("Set of prime numbers:", prime_numbers)
-print("Length of the set:", len(prime_numbers))
+prime_number= {2, 3, 5, 7, 11, 13, 17, 19, 23, 29}
+print("Set of prime numbers:", prime_number)
+print("Length of the set:", len(prime_number))
