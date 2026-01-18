fb11c29d68c27371f13a5d848560eb11b2b91828
machphy <rajeevsharmamachphy@gmail.com>
2026-01-18 18:22:26 +0530

    newQuarantine or delete the phishing email from the mailbox

---
+++ Airtel_udemy/insurance.py
@@ -0,0 +1,28 @@
+# Create a python file that asks an insurance company the following inputs:
+
+
+
+# Insurance company name
+
+# Number of employees
+
+# location (city, OR country, OR state)
+
+# Lowest price for a policy
+
+# The highest price for a policy
+
+# The output will consist of a prompt almost like an ad. For example:
+
+# "We are Company LLC located in India. Our 55 employees can help you find the policy that is right for you with policies ranging from $15 to $300 per month!"
+
+# Questions for this assignment
+# What was the most challenging part of this for you?
+
+company_name=input ("enter u com name")
+num_emp_in_comp=("ent_no_of_emp")
+location=("enter location ")
+lowest_price = input("Enter the lowest price for a policy: ")
+highest_price = input("Enter the highest price for a policy: ")
+
+# output
