6f8ff39080d9eaddee3666eb8ea5fc3e5dfe3adb
machphy <rajeevsharmamachphy@gmail.com>
2026-01-20 23:42:54 +0530

    newok

--- Airtel_udemy/new.py
+++ Airtel_udemy/new.py
@@ -1,1 +1,12 @@
-new=('new')
+new=('new')kB]
+Get:30 http://in.archive.ubuntu.com/ubuntu noble-updates/multiverse amd64 Components [940 B]
+Get:31 http://in.archive.ubuntu.com/ubuntu noble-backports/main amd64 Components [7,300 B]
+Get:32 http://in.archive.ubuntu.com/ubuntu noble-backports/restricted amd64 Components [216 B]
+Get:33 http://in.archive.ubuntu.com/ubuntu noble-backports/universe amd64 Components [10.5 kB]
+Get:34 http://in.archive.ubuntu.com/ubuntu noble-backports/multiverse amd64 Components [212 B]
+Fetched 5,731 kB in 7s (863 kB/s)                                                      
+Reading package lists... Done
+Building dependency tree... Done
+Reading state information... Done
+31 packages can be upgraded. Run 'apt list --upgradable' to see them.
+# rajeev@rajeevmachphy:~$ 
